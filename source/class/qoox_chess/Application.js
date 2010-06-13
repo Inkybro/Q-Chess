024dd8e62fd25163178c0da0678749693d9c0893
Petrea Stefan Corneliu <Administrator@.(none)>
2010-06-13 16:40:02 +0300

    added pawn and knight legal moves

--- source/class/qoox_chess/Application.js
+++ source/class/qoox_chess/Application.js
@@ -76,6 +76,7 @@ qx.Class.define("qoox_chess.Application",
 
 
       this._active = null;
+      var typeregex = new RegExp("(rock|queen|bishop|knight|king)","");
 
       for (var x=0; x<8; x++)
       {
@@ -86,17 +87,68 @@ qx.Class.define("qoox_chess.Application",
 
 
 
+          var piece;
           var composite = new qx.ui.container.Composite(new qx.ui.layout.Grow());
           box.add(composite,{row: y, column: x});
 	  var newcell   = this.getNewWidget( (((x%2)+(y%2))%2==0) ? "black":"white" , x , y);
+
+          // (x,y) coordinates on the board
+          newcell.xc = x;
+          newcell.yc = y;
+
           newcell.belongingComposite = composite;// store it here because I haven't found the method for this
                                                  // in the docs yet(need to look some more)
+                                                 //
           newcell.setDroppable(true);// we can drop a piece on any of the cells of the 8x8 board
 
 
+          // the fact that the drop listener is installed only on
+          // the cells, this also eliminates the possibility of having 2 pieces in one cell
+          // (you can only drop on a cell, but if the cell already has an image on it,
+          // then the cell is beneath the image and at the same time the image does not have a drop
+          // listener installed on it)
           newcell.addListener("drop", function(e) {
                   //TODO: legal chess moves
-                  this.belongingComposite.add(e.getRelatedTarget());
+
+                  var moved_piece = e.getRelatedTarget();
+                  var spot        = this;//where it is placed
+                  var oldspot = moved_piece.oldspot;
+
+                  var legal = false;
+
+                  //alert(moved_piece.player);
+                  //alert(moved_piece.piece_type);
+
+                  if(moved_piece.player == "black") {
+                      if(moved_piece.piece_type == "pawn") {
+                            if(
+                                oldspot.yc - spot.yc == 1 &&
+                                oldspot.xc ==spot.xc
+                                )//one row higher
+                                legal = true;
+                      };
+
+                  };
+
+                  //no need to check dx or dy outside bounds because
+                  //they can only drop on cells which are already in bounds
+                  if(moved_piece.piece_type == "knight") {
+                      var dx = Math.abs(oldspot.xc - spot.xc);
+                      var dy = Math.abs(oldspot.yc - spot.yc);
+                      if( 
+                          (dx == 2 && dy==1)||
+                          (dx == 1 && dy==2)
+                        )
+                          legal = true;
+                  };
+
+
+                  if(legal) {
+                      //alert("moving");
+                      spot.belongingComposite.add(moved_piece);
+                      moved_piece.composite = spot.belongingComposite;
+                      moved_piece.oldspot = spot;
+                  };
           });
 
 
@@ -104,43 +156,64 @@ qx.Class.define("qoox_chess.Application",
 
           if(y>1 && y<6)
               continue;
+          // from here onwards y can only be in {0,1,6,7}
 
 
           //y==1 or y==6
-          //peons
-          var piece     = new qx.ui.basic.Image(
+          //pawns
+
+
+
+          if(y==0||y==7) {
+              // knights/rooks/bishop/king/queen
+              // arranged so that they are in reverse order in comparison to the other player
+              var img_name = 
+                  (
+                   y==0 
+                   ? "w" + backline[7-x]
+                   :       backline[x]
+                  );
+
+              piece = new qx.ui.basic.Image( "resource/qoox_chess/" + img_name );
+
+              if(img_name) {
+                  piece.piece_type = typeregex.exec(img_name);
+                  if(piece.piece_type) {
+                      piece.piece_type = piece.piece_type[0];
+                      if(y == 0) {
+                          piece.player = "white";
+                      } else {//y==7
+                          piece.player = "black";
+                      };
+                  };
+              };
+          } else {
+              piece     = 
+                  new qx.ui.basic.Image(
                           "resource/qoox_chess/"+
-                            (
-                             y==1
-                             ?"wpeon.PNG"
-                             : "peon.PNG"
-                            )
-                         );
+                              (
+                               y==1
+                               ?"wpawn.PNG"
+                               : "pawn.PNG"
+                              )
+                          );
+              if(y==1) {
+                  piece.player = "white";
+              } else {//y==6
+                  piece.player = "black";
+              };
 
+              piece.piece_type = "pawn";
+          };
 
-
-          // knights/rooks/bishop/king/queen
-          // arranged so that they are in reverse of the other player
-          if(y==0||y==7)
-              piece = new qx.ui.basic.Image(
-                      "resource/qoox_chess/" + 
-                          (
-                           y==0 
-                           ? "w" + backline[7-x]
-                           :       backline[x]
-                          )
-                      );
 
           piece.setDraggable(true);
+          piece.addListener(
+                            "dragstart", 
+                            function(e) { e.addAction("move"); }
+                           );
 
-
-          piece.addListener("dragstart", function(e) {
-                  e.addAction("move");
-          });
-
-
-
-
+          piece.oldspot = newcell;
           composite.add(piece);
         }
       }
